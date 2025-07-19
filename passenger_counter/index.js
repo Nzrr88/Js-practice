b90574c90548e1b6006024dc65313b0166e2d124
Nzrr <god.nizaryt10@gmail.com>
2025-07-19 19:41:34 +0100

    Finished the project

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,6 +1,10 @@
 const countEl = document.getElementById("count-el");
 const btnInc = document.querySelectorAll("button")[0];
 const btnDec = document.querySelectorAll("button")[1];
+const btnSave = document.getElementById("btnSave");
+const saveEl = document.querySelector("p");
+
+let Saving = [];
 
 btnInc.onclick = function () {
   let current = parseInt(countEl.innerText);
@@ -15,3 +19,19 @@ btnDec.onclick = function () {
     alert("No passenger exists");
   }
 };
+
+function save (){
+    let current = countEl.innerText
+    if (parseInt(current)){
+        Saving.push(current)
+        saveEl.textContent+= current + " - " 
+        countEl.innerText = 0;
+    }
+
+}
+function reset (){
+   countEl.innerText = 0;
+   Saving = []
+   saveEl.textContent = "Previous entries: "
+
+}
